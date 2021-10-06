1cd5fb382d4a2c29fd497ea974f329db4c35b739
Humoyun2003 <abduqodirovh595>
2021-10-06 14:54:02 +0500

    Add Sort and Filter Function

--- js/functions.js
+++ js/functions.js
@@ -1,11 +1,12 @@
 // Render Films Array
-
 function renderFilms(arr, node) {
 	elBoxContent.innerHTML = null;
 
 	arr.forEach(film => {
 		const newBox = document.createElement('div');
 		const newContent = document.createElement('div');
+		const newLike = document.createElement('div');
+		const newHeart = document.createElement('i');
 		const newImage = document.createElement('img');
 		const newHeading = document.createElement('h3');
 		const newParagraph = document.createElement('p');
@@ -15,7 +16,7 @@ function renderFilms(arr, node) {
 
 		newHeading.textContent = film.title;
 		newParagraph.textContent =
-			film.overview.split(' ').slice(0, 25).join(' ') + '...';
+		film.overview.split(' ').slice(0, 25).join(' ') + '...';
 		newTime.textContent = normalizeDate(film.release_date);
 		newBtn.textContent = 'Watch Now';
 
@@ -27,6 +28,8 @@ function renderFilms(arr, node) {
 
 
 		newBox.setAttribute('class', 'box');
+		newLike.setAttribute('class', 'like');
+		newHeart.setAttribute('class', 'far fa-heart like-icon');
 		newImage.setAttribute('src', film.poster);
 		newImage.setAttribute('alt', film.title + ' poster');
 		newContent.setAttribute('class', 'content');
@@ -39,10 +42,12 @@ function renderFilms(arr, node) {
 		newContent.appendChild(newGenreList);
 		newContent.appendChild(newTime);
 		newContent.appendChild(newBtn);
+		newLike.appendChild(newHeart);
+		newBox.appendChild(newLike);
 		newBox.appendChild(newImage);
 		newBox.appendChild(newContent);
 
-		node.appendChild(newBox);
+		node.appendChild(newBox);film
 	})
 }
 
@@ -59,10 +64,83 @@ function editGenreOptions(arr, select) {
 			}
 		}
 	}
+	optionGanres.sort((a, b) => {
+		if(a > b) {
+			return 1;
+		}
+		if(a < b) {
+			return -1;
+		}
+		return 0;
+	});
 	for (let option of optionGanres) {
 		const newOption = document.createElement('option');
 		newOption.value = option;
 		newOption.textContent = option;
 		select.appendChild(newOption);
 	}
+}
+// Sort function working for Select
+const sortFunctions = {
+	0: (a, b) => {
+		if(a.title > b.title) {
+			return 1;
+		}
+		if(a.title < b.title) {
+			return -1;
+		}
+		return 0;
+	},
+	1: (a, b) => {
+		if(a.title > b.title) {
+			return -1;
+		}
+		if(a.title < b.title) {
+			return 1;
+		}
+		return 0;
+	},
+	2: (a, b) => {
+		if(a.release_date > b.release_date) {
+			return -1;
+		}
+		if(a.release_date < b.release_date) {
+			return 1;
+		}
+		return 0;
+	},
+
+	3: (a, b) => {
+		if(a.release_date > b.release_date) {
+			return 1;
+		}
+		if(a.release_date < b.release_date) {
+			return -1;
+		}
+		return 0;
+	}
+}
+
+
+
+// All film Sort and Filter function
+function filmsSortFn(evt) {
+	evt.preventDefault();
+
+	var filteredFilm = [];
+	
+	if (elSelect.value == 'all') {
+		filteredFilm = films;
+	}
+	else {
+		filteredFilm = films.filter(film => film.genres.includes(elSelect.value))
+	}
+	
+	filteredFilm.sort(sortFunctions[elSelectSort.value]);
+	
+	elSpan.textContent = elSelect.value;
+
+	const newRegx = new RegExp(elSearchInput.value, 'gi');
+	filteredFilm = filteredFilm.filter(film => film.title.match(newRegx));
+	renderFilms(filteredFilm, elBoxContent);
 }
--- js/main.js
+++ js/main.js
@@ -1,19 +1,30 @@
-const elSelect = document.querySelector('.select');
-const elForm = document.querySelector('.form');
+const elSelect = document.querySelector('.select-genre');
+const elSelectSort = document.querySelector('.select-sort');
+const elForm = document.querySelector('.form-search');
 const elSpan = document.querySelector('.span');
 const elBoxContent = document.querySelector('.box-container');
+const elSearchInput = document.querySelector('.input-search');
+
 
 editGenreOptions(films, elSelect);
 
-elForm.addEventListener('submit', (evt) => {
-	evt.preventDefault();
-	let filteredFilm = [];
-	if(elSelect.value == 'all') {
-		filteredFilm = films;
-	}
-	else {
-		filteredFilm = films.filter(film => film.genres.includes(elSelect.value));
-	}
-	renderFilms(filteredFilm, elBoxContent);
-	elSpan.textContent = elSelect.value;
-})
+renderFilms(films.sort(sortFunctions[0]), elBoxContent);
+
+elForm.addEventListener('input', filmsSortFn)
+
+
+
+// const elLike = document.querySelector('.like');
+// const elHeart = document.querySelector('.like-icon');
+// let sum = false;
+
+// elLike.addEventListener('click', (e) => {
+// 	if (sum === false) {
+// 		elHeart.classList.toggle('fas',);
+// 		sum = true;
+// 	}
+// 	if (sum === true) {
+// 		elHeart.classList.toggle('far');
+// 		sum = false;
+// 	}
+// });
